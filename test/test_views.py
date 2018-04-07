ecbc7bd7f41dbdd8c90e7610fc48a87b30dfafa5
brzeczunio <xxx@x.pl>
2018-04-07 13:20:41 +0200

    Poprawa testu

--- test/test_views.py
+++ test/test_views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import unittest
 from hello_world import app
 from hello_world.formater import SUPPORTED
@@ -14,4 +16,4 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEquals('{ "imie":"Damian", "mgs":"Hello World!"}', rv.data)
+        self.assertEquals('{ "imie":"Damian", "mgs":"jesteś zajebisty! :)"}', rv.data)
