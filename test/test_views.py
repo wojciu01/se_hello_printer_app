d10a5b17c24e5b25e1124ec38c14342931f0d130
brzeczunio <xxx@x.pl>
2018-04-07 13:27:30 +0200

    Poprawa lint i testu

--- test/test_views.py
+++ test/test_views.py
@@ -16,4 +16,5 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEquals('{ "imie":"Damian", "mgs":"jesteś zajebisty! :)"}', rv.data)
+        expected = '{ "imie":"Damian", "mgs":"jesteś zajebisty! :)"}'
+        self.assertEquals(expected, rv.data)
